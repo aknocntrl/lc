f470b354009fcaeeb448b748ad31628ab29bee72
Akshara Mishra <aksharamishra02.0@gmail.com>
2026-07-30 18:50:46 +0530

    Time: 0 ms (100%), Space: 62.9 MB (49.75%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,6 +1,6 @@
-class Solution {
+class Solution{
 public:
-    int maxArea(vector<int>& height) {
+    int maxArea(vector<int>& height){
         int l=0;
         int r=height.size()-1;
         int ans=0;
