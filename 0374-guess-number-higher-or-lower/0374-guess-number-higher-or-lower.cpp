13599059f7cc006606d15e57afb209b083a0c379
Akshara Mishra <aksharamishra02.0@gmail.com>
2026-08-13 23:49:35 +0530

    Time: 0 ms (100%), Space: 7.9 MB (75.02%) - LeetHub

---
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -0,0 +1,15 @@
+class Solution{
+public:
+    int guessNumber(int n){
+        long long low=1,high=n;
+        while(low<=high){
+            long long mid=low+(high-low)/2;
+            int res=guess(mid);
+            if(res==0){
+                return mid;}
+            else if(res==-1){
+                high=mid-1;}
+            else{
+                low=mid+1;}}
+        return -1;}
+};
